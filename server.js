380eabf3dc698d840b09e8d889485af1672d7d57
akshat <akshat.agrawal@w3villa.com>
2025-08-19 16:23:00 +0530

    packakge.json changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 const chatSocket = require("./src/socket/chatSocket");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 const server = http.createServer(app);
 
 const io = new Server(server, {
